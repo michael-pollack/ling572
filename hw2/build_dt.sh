dd2f45c871e146ef7fab929822a3d4d0f2d951e5
Michael Pollack <mppollack@Michaels-MacBook-Pro.local>
2025-01-24 17:14:46 -0800

    ok done for real

--- hw2/build_dt.sh
+++ hw2/build_dt.sh
@@ -9,4 +9,4 @@ model_file=$5
 out_path=$6
 
 source ~/miniconda3/bin/activate "/mnt/dropbox/24-25/572/envs/570" # example: "/mnt/dropbox/23-24/WIN571/envs/571"
-python build_dt.py --train $training_data --test $test_data --max_dept $max_depth --min_gain $min_gain --modal $model_file --output $out_path
+python build_dt.py --train $training_data --test $test_data --max_dept $max_depth --min_gain $min_gain --model $model_file --output $out_path
