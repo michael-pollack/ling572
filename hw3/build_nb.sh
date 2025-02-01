bb8f3c69d2b2296367598a6280cc6b558f5422d4
Michael Pollack <mppollack@Michaels-MacBook-Pro.local>
2025-01-31 18:33:07 -0800

    one more time, one more time! we're gonna celebraaate oh yeah alright we're gonna celebrate

--- hw3/build_nb.sh
+++ hw3/build_nb.sh
@@ -9,5 +9,5 @@ model=$5
 output=$6
 
 
-#source ~/miniconda3/bin/activate "/mnt/dropbox/24-25/572/envs/570" # example: "/mnt/dropbox/23-24/WIN571/envs/571"
+source ~/miniconda3/bin/activate "/mnt/dropbox/24-25/572/envs/570" # example: "/mnt/dropbox/23-24/WIN571/envs/571"
 python build_nb.py --train $training_data --test $test_data --model $model --output $output --cpdelta $cpdelta --prdelta $prdelta
