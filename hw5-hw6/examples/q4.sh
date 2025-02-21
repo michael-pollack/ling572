fe9fdf41f4a181e878b7f116c160162be4d6c312
Michael Pollack <uwmpp@patas.ling.washington.edu>
2025-02-21 09:10:04 -0800

    patas-ified

--- hw5-hw6/examples/q4.sh
+++ hw5-hw6/examples/q4.sh
@@ -1,27 +1,26 @@
 #!/bin/sh
 
-/nopt/python-3.6/bin/python3.6 grad_desc.py g1 0.01 200 -1.8 > E1
-/nopt/python-3.6/bin/python3.6 grad_desc.py g1 0.01 200 1.8 > E2
+./grad_desc.sh g1 0.01 200 -1.8 > q4/E1
+./grad_desc.sh g1 0.01 200 1.8 > q4/E2
 
-/nopt/python-3.6/bin/python3.6 grad_desc.py g2 0.01 200 -1.8 > E3
-/nopt/python-3.6/bin/python3.6 grad_desc.py g2 0.01 200 1.8 > E4
+./grad_desc.sh g2 0.01 200 -1.8 > q4/E3
+./grad_desc.sh g2 0.01 200 1.8 > q4/E4
 
-/nopt/python-3.6/bin/python3.6 grad_desc.py g3 0.01 200 -1.8 > E5
-/nopt/python-3.6/bin/python3.6 grad_desc.py g3 0.01 200 1.8 > E6
+./grad_desc.sh g3 0.01 200 -1.8 > q4/E5
+./grad_desc.sh g3 0.01 200 1.8 > q4/E6
+./grad_desc.sh g4 0.01 200 -1.8 > q4/E7
+./grad_desc.sh g4 0.01 200 1.8 > q4/E8
 
-/nopt/python-3.6/bin/python3.6 grad_desc.py g4 0.01 200 -1.8 > E7
-/nopt/python-3.6/bin/python3.6 grad_desc.py g4 0.01 200 1.8 > E8
+./grad_desc.sh g5 0.01 200 -1.8 > q4/E9
+./grad_desc.sh g5 0.01 200 1.8 > q4/E10
 
-/nopt/python-3.6/bin/python3.6 grad_desc.py g5 0.01 200 -1.8 > E9
-/nopt/python-3.6/bin/python3.6 grad_desc.py g5 0.01 200 1.8 > E10
+./grad_desc.sh g6 0.01 200 -1.8 > q4/E11
+./grad_desc.sh g6 0.01 200 0.77 > q4/E12
+./grad_desc.sh g6 0.01 200 0.05 > q4/E13
+./grad_desc.sh g6 0.01 200 0.3 > q4/E14
+./grad_desc.sh g6 0.01 200 0.76 > q4/E15
 
-/nopt/python-3.6/bin/python3.6 grad_desc.py g6 0.01 200 -1.8 > E11
-/nopt/python-3.6/bin/python3.6 grad_desc.py g6 0.01 200 0.77 > E12
-/nopt/python-3.6/bin/python3.6 grad_desc.py g6 0.01 200 0.05 > E13
-/nopt/python-3.6/bin/python3.6 grad_desc.py g6 0.01 200 0.3 > E14
-/nopt/python-3.6/bin/python3.6 grad_desc.py g6 0.01 200 0.76 > E15
-
-/nopt/python-3.6/bin/python3.6 grad_desc.py f1 0.01 200 1 2 > E16
-/nopt/python-3.6/bin/python3.6 grad_desc.py f2 0.01 200 1 2 > E17
-/nopt/python-3.6/bin/python3.6 grad_desc.py f3 0.01 200 1 2 > E18
-/nopt/python-3.6/bin/python3.6 grad_desc.py f4 0.01 200 1 2 > E19
+./grad_desc.sh f1 0.01 200 1 2 > q4/E16
+./grad_desc.sh f2 0.01 200 1 2 > q4/E17
+./grad_desc.sh f3 0.01 200 1 2 > q4/E18
+./grad_desc.sh f4 0.01 200 1 2 > q4/E19
--- hw5-hw6/grad_desc.sh
+++ hw5-hw6/grad_desc.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-#source ~/miniconda3/bin/activate "/mnt/dropbox/24-25/572/envs/570" # example: "/mnt/dropbox/23-24/WIN571/envs/571"
+source ~/miniconda3/bin/activate "/mnt/dropbox/24-25/572/envs/570" # example: "/mnt/dropbox/23-24/WIN571/envs/571"
 python3 grad_desc.py --func_name $1 --learning_rate $2 --iteration_number $3 --x1_val $4 ${5:+--x2_val "$5"}
